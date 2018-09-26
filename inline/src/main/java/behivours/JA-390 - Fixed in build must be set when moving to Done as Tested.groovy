e2ed8559cea25bad721f4bead7c021db046d06b3
Michaelsh <shechter.mm@gmail.com>
2018-09-26 23:32:47 +0300

    Update from Jira prod

--- inline/src/main/java/behivours/JA-390 - Fixed in build must be set when moving to Done as Tested.groovy
+++ inline/src/main/java/behivours/JA-390 - Fixed in build must be set when moving to Done as Tested.groovy
@@ -20,7 +20,9 @@ List issueTypesArr = ['Epic','Story','SubTask','Bug']
 log.warn("<---|IssueKey : ${underlyingIssue.getKey()} | issueType is : ${issueType.name} | workflow Destination is : ${workflowDestination} | issue Resolution is : ${resolution}--->")
 //if one of the users is :oracle_sc, jenkins,JiraAutomation, exit
 if(!(currentUser == "oracle_sc" || currentUser == "jenkins" || currentUser == "JiraAutomation")) {
-    if ( issueType.name == 'Bug' && workflowDestination == 'QA')
+    if (issueType.name == 'Bug' && workflowDestination == 'QA')
         fixInBuild.setRequired(true)
-    else if(workflowDestination =='Done' && issueType in(issueTypesArr) && resolution =='Tested' )
+    else if (workflowDestination == 'Done' && issueType in (issueTypesArr) && resolution == 'Tested')
         fixInBuild.setRequired(true)
+
+}
